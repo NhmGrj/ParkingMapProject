6eb0116fdfe340bc5a57d1fd33a2f8fda096aa74
NhmGrj <nhm.grd@gmail.com>
2017-05-17 07:12:47 +0200

    Fixtures behavior fixed

--- src/ParkingMapBundle/DataFixtures/ORM/LoadSlotsData.php
+++ src/ParkingMapBundle/DataFixtures/ORM/LoadSlotsData.php
@@ -10,36 +10,50 @@ class LoadSlotsData implements FixtureInterface {
 
     public function load(ObjectManager $manager)
     {
-        // $timesArray = [];
+        //Time Specification ISO 8601.
+        //https://en.wikipedia.org/wiki/ISO_8601#Durations
+        $tenHours = "PT10H";
+        $fifteenMinutes = "PT15M"
 
-        // hoursArray instanciation
-        // for ($h=0; $h <= 24; $h++) {
-        //     $timesArray[] = $time;
-        // }
-
-        //Set up 10 slots
-        for ($i=0; $i < 10; $i++) {
+        //Set up 10 slots, with potential state update every 10 min
+        //from 10h than currentTime for each slots.
+        for ($i=0; $i <= 10; $i++) {
             $time = new \DateTime();
+            $time = $time->sub(new \DateInterval($tenHours));
             $slot = new Slots();
 
-            //Set up states data related to each slot, for each last 24h
-            for ($h=0; $h <= 24; $h++) {
-                $time->sub(new \DateInterval('PT1H'));
+            //Set up states data related to each slot
+            for ($it=0; $it <= 40; $it++) {
                 $state = new State();
                 $state->setDate($time);
-                $state->setState(rand(0, 1));
+
+                //Let's simulate that slots state can change
+                // every 15 min.
+                if($it == 0) {
+                    // Let's assume all slots were free at the beginning
+                    $state->setState(true);
+                    $lastState = $state->getState();
+                } else {
+                    // If it is not the first iteration, let's
+                    // randomly change the state or not
+                    // if the state change, it means someone leave,
+                    // or occupied the the slot.
+                    $state->setLastState($lastState);
+                    $state->setState(rand(0, 1) ? !$lastState : $lastState);
+                    $lastState = $state->getState();
+                }
                 $state->setSlot($slot);
                 $manager->persist($state);
-                $manager->flush();
+                // $manager->flush();
 
-
-                $slot->getStates()->add($state);
+                $time->add(new \DateInterval($fifteenMinutes));
             }
+
             $manager->persist($slot);
-            $manager->flush();
         }
 
 
+        $manager->flush();
 
     }
 }
