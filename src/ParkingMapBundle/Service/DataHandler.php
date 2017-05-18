de72a5d068d89090c329fd62c158f736d49947c5
NhmGrj <nhm.grd@gmail.com>
2017-05-18 23:45:18 +0200

    Services TimeHandler and Datahandler added

---
+++ src/ParkingMapBundle/Service/DataHandler.php
@@ -0,0 +1,81 @@
+<?php
+namespace ParkingMapBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+use ParkingMapBundle\Service\TimeHandler;
+// use ParkingMapBundle\Repository\StateRepository;
+
+class DataHandler {
+    private $em;
+    private $th;
+
+    private $slotsRepository;
+    private $stateRepository;
+
+    private $slotsNb;
+
+    public function __construct(EntityManager $em, TimeHandler $th) {
+
+        $this->em = $em;
+        $this->th = $th;
+        $this->slotsRepository = $this->em->getRepository("ParkingMapBundle:Slots");
+        $this->stateRepository = $this->em->getRepository("ParkingMapBundle:State");
+    }
+
+    private function setSlotsNb($nb) {
+        $this->slotsNb = $nb;
+    }
+
+    public function getSlotsNb() {
+        return $this->slotsNb;
+    }
+
+    public function getEntriesByHoursSpan($hoursNb, $hoursSpan) {
+        $hoursSpanArray = $this->th->getSpanHoursArray($hoursNb, $hoursSpan);
+
+        foreach ($hoursSpanArray as $key => $hour) {
+            $slots = $this->slotsRepository->findAllStatesByHoursSpan($hour['current'], $hour['prevHour']);
+            $this->setSlotsNb(count($slots));
+
+
+            foreach($slots as $slot) {
+                $hoursSpanArray[$key]['counter'][$slot->getId()] = array(
+                    'entry' => 0,
+                    'exit' => 0
+                );
+                $hoursSpanArray[$key]['totalEntries'] = 0;
+                $hoursSpanArray[$key]['totalExits'] = 0;
+
+                foreach($slot->getStates() as $state) {
+
+                    //If the state change
+                    //it means either someone got or leaved the slot
+                    if($state->getState() != $state->getLastState()){
+                        if(!$state->getState()) {
+                            // var_dump('One exit');
+                            $hoursSpanArray[$key]['counter'][$slot->getId()]['exit']++;// Count one exit for that slot for that hour
+                        } else {
+                            // var_dump('One Entry');
+
+                            $hoursSpanArray[$key]['counter'][$slot->getId()]['entry']++;// Count one entry for that hour for that slot
+                        }
+                    }
+                }
+
+                //Sum of all entries/exits for an hour span
+                foreach($hoursSpanArray[$key]['counter'] as $values) {
+                    $hoursSpanArray[$key]['totalEntries']+= $values['entry'];
+                    $hoursSpanArray[$key]['totalExits']+= $values['exit'];
+                }
+
+
+            }
+            $this->em->clear();
+        }
+
+        return $hoursSpanArray;
+    }
+
+
+    
+}
---
+++ src/ParkingMapBundle/Service/TimeHandler.php
@@ -0,0 +1,41 @@
+<?php
+namespace ParkingMapBundle\Service;
+
+class TimeHandler {
+    public $currentTime;
+
+
+
+    public function __construct() {
+        $this->currentTime = new \DateTime();
+    }
+
+    public function getTime() {
+        return $this->currentTime;
+    }
+
+    //Time Specification ISO 8601.
+    //https://en.wikipedia.org/wiki/ISO_8601#Durations
+    public function subHours($hoursNb) {
+        return $this->currentTime->sub(new \DateInterval("PT".$hoursNb."H"));
+    }
+
+    public function addMins($minsNb) {
+        return $this->currentTime->add(new \DateInterval("PT".$minsNb."M"));
+    }
+
+    public function getSpanHoursArray($hoursNb, $hoursSpan) {
+        $hoursArray = [];
+        $time = new \DateTime();
+        for ($i=0; $i <= $hoursNb; $i++) {
+            $hoursArray[] = array(
+                'current' => new \DateTime($time->format("Y-m-d H:i:s")),
+                'prevHour' => new \DateTime($time->sub(new \DateInterval("PT".$hoursSpan."H"))->format("Y-m-d H:i:s")),
+
+            );
+        }
+        return $hoursArray;
+    }
+
+
+}
