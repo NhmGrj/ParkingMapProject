d59af1f9f877aebda47b21323da05ba4d229f39e
NhmGrj <nhm.grd@gmail.com>
2017-05-19 22:09:44 +0200

    bootstrap installed, data on traffic by hours displayed, free slot nb displayed, nb of places also displayed

--- src/ParkingMapBundle/Controller/DefaultController.php
+++ src/ParkingMapBundle/Controller/DefaultController.php
@@ -12,6 +12,20 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('ParkingMapBundle:Default:index.html.twig');
+        $dataHandler = $this->container->get('pkm.data_handler');
+        $em = $this->container->get('doctrine');
+        $slotsRepository = $em->getRepository('ParkingMapBundle:Slots');
+        $nbHour = 10;
+        $hourSpan = 1;
+
+        $trafficByHoursSpan = $dataHandler->getEntriesByHoursSpan($nbHour, $hourSpan);
+        $slotsNb = $slotsRepository->getSlotsNb();
+        $freeSlotsNb = $slotsRepository->getFreeSlotsNb();
+
+        return $this->render('ParkingMapBundle:Default:index.html.twig', array(
+            "trafficByHoursSpan"  => $trafficByHoursSpan,
+            "slotsNb"             => $slotsNb,
+            "freeSlotsNb"         => $freeSlotsNb
+        ));
     }
 }
